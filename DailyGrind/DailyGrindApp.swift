dc9aa15ed1bc53e96754781d08aa1f44463ce98c
MojoMoogly574 <jdweese574@gmail.com>
2022-03-30 15:00:51 -0500

    Initial Commit

---
+++ DailyGrind/DailyGrindApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyGrindApp.swift
+//  DailyGrind
+//
+//  Created by Joseph DeWeese on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyGrindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
